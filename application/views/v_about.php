d30deec05b32dd8f2f69113fc2ee4f0124ba7bb6
Jametxxx <hoioopo87@gmail.com>
2023-10-13 16:09:38 +0700

    Menambahkan Tugas

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Ferrel</li>
+ <li>Nama Belakang : Ale</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Cawas</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Klaten</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Sepak bola</li>
+ <li>Dan lain lain</li>
+ </ul>
+</section>
